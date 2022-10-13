6940b72f4aa5655869bdd4047f5f10a47f2a665b
bdavs3 <bdavs3000@gmail.com>
2022-10-13 16:27:33 -0600

    Enhance GDPR validation by pulling all relevant views from Snowflake

--- dags/snowflake-minimal-gdpr-validation.py
+++ dags/snowflake-minimal-gdpr-validation.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-
-DAG_ID = "snowflake-minimal-gdpr-validation"
+from airflow.decorators import task
 
 SNOWFLAKE_CONN_ID = 'snowflake_conn'
 SNOWFLAKE_ROLE = 'ACCOUNTADMIN'
@@ -12,35 +11,47 @@ SNOWFLAKE_DATABASE = 'SOVRN'
 SNOWFLAKE_SCHEMA = 'PUBLIC'
 SNOWFLAKE_WAREHOUSE = 'COMPUTE_WH'
 
-SNOWFLAKE_VIEW = 'VW_WEB_SHARETHIS_GDPR'
 SQL_TEXT = "SHOW VIEWS LIKE '%_GDPR'"
 
-def validate_gdpr(cursor):
-    for row in cursor:
-        ddl_string = str(row["text"]) #text column contains DDL used to create the view
-        ddl_string = ddl_string.translate({ord(c): None for c in string.whitespace}).lower() #Remove all whitespace and convert to lc
-        print(ddl_string)
+def get_ddl(results):
+    ddl_dict={}
+
+    for row in results:
+        view_name = str(row["name"])
+        ddl_string = str(row["text"])
+        ddl_string = ddl_string.translate( #Remove all whitespace and convert to lowercase
+            {ord(c): None for c in string.whitespace}
+        ).lower()
+
+        ddl_dict[view_name] = ddl_string
+    
+    return ddl_dict
+
+@task
+def check_for_gdpr_validation(ti=None):
+    ddl_dict = ti.xcom_pull(task_ids="get_ddl_task", key="return_value")
+
+    for view_name, ddl_string in ddl_dict.items():
         if "gdpr=true" in ddl_string:
-            print("GDPR Check is present")
+            print(view_name, "GDPR Check is present")
         else:
-            print("GDPR check not present") 
-        
+            print(view_name, "GDPR Check not present")
+
 with DAG(
-    DAG_ID,
-    default_args={'snowflake_conn_id': f'{SNOWFLAKE_CONN_ID}'},
+    'snowflake-minimal-gdpr-validation',
     start_date=datetime(1970, 1, 1),
     catchup=False,
 ) as dag:
-    gdpr_validation_task = SnowflakeOperator(
-        task_id='gdpr_validation_task',
-        sql=f'{SQL_TEXT}',
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA,
-        role=SNOWFLAKE_ROLE,
-        handler=validate_gdpr
-    )
+    get_ddl_task = SnowflakeOperator(
+            snowflake_conn_id = 'snowflake_conn',
+            task_id='get_ddl_task',
+            sql=f'{SQL_TEXT}',
+            warehouse=SNOWFLAKE_WAREHOUSE,
+            database=SNOWFLAKE_DATABASE,
+            schema=SNOWFLAKE_SCHEMA,
+            role=SNOWFLAKE_ROLE,
+            handler=get_ddl,
+            dag=dag
+        )
 
-    gdpr_validation_task
-
-dag
+    get_ddl_task >> check_for_gdpr_validation()
