26dadee81989c0b016cee9418326b2baf15e5b0a
bdavs3 <bdavs3000@gmail.com>
2022-10-19 15:00:31 -0600

    Use staticmethod decorators in util

--- dags/util/util.py
+++ dags/util/util.py
@@ -1,10 +1,12 @@
 class Util:
+    @staticmethod
     def parse_for_key(url):
         key_start = url.find('/snowflake')
         key = url[key_start:]
 
         return key
 
+    @staticmethod
     def get_view_from_stage(stage_name):
         if 'DAILY' in stage_name:
             return stage_name.replace('STG_DAILY', 'VW')
