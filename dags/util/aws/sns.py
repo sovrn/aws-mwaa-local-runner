c6e963c561fc63f7ba7e81ccfdb897c449dd9f39
bdavs3 <bdavs3000@gmail.com>
2022-10-24 15:10:59 -0600

    Add a function to the sns class that helps separate logic from callers

--- dags/util/aws/sns.py
+++ dags/util/aws/sns.py
@@ -1,7 +1,17 @@
 from airflow.providers.amazon.aws.hooks.sns import AwsSnsHook
 
-#AWS_CONN_ID = 'aws_ops_access'
-
 class SNS:
     def __init__(self):
         self.hook = AwsSnsHook()
+
+    def publish_to_target(self, target_arn, alarm_name, new_state_value, new_state_reason, description):
+        self.hook.publish_to_target(
+            target_arn=f"{target_arn}",
+            message=f"""{{
+                "AlarmName":"{alarm_name}",
+                "NewStateValue":"{new_state_value}",
+                "NewStateReason":"{new_state_reason}",
+                "StateChangeTime":"1970-01-01T01:00:00.000Z",
+                "AlarmDescription":"{description}"
+            }}"""
+        )
